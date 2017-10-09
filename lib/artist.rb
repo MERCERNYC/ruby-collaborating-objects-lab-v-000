65776ba491ddbc0e1c5e62bb81930f41ece038c8
Juliana Fogg <julianafogg@gmail.com>
2017-10-09 19:22:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   # def self.find_or_create_by_name(name)
   #     self.find(name) ? self.find(name) : self.create(name)
   #   end
-    
+
   def self.find_or_create_by_name(name) 
     @@all.each do |artist|
       if artist.name == name
